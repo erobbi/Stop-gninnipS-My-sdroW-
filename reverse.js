97312595a2f845f63d05b0f160056bdb5dfe977d
Eli Robbins <49540422+erobbi@users.noreply.github.com>
2021-11-19 10:23:26 -0600

    added solution

---
+++ reverse.js
@@ -0,0 +1,9 @@
+function spinWords(string){
+  let arr = string.split(" ");
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i].length >= 5) {
+      arr[i] = arr[i].split("").reverse().join("")
+    }
+  }
+  return arr.join(" ")
+}
